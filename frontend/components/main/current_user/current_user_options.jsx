b8ee9d3d68289ba8300b49694343502d63d3a789
Vincent Ahn <jmahn97@hotmail.com>
2021-07-05 18:21:38 -0400

    Style: Created basic main application interface styling framework

--- frontend/components/main/current_user/current_user_options.jsx
+++ frontend/components/main/current_user/current_user_options.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class CurrentUserOptions extends React.Component{
   render(){
     return(
-      <div>
+      <div className="current-user-options">
         <h1>Hello, {this.props.username}</h1>
         <img src={this.props.profileUrl} alt="Profile Photo not Found" />
         <button onClick={this.props.logout}>Logout</button>
--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import CurrentUserOptionsContainer from './current_user/current_user_options_container'
 
 const Main = () => (
-  <div>
-    <h1>This is the main page!</h1>
-    <CurrentUserOptionsContainer />
+  <div className="main">
+    <div className="column-1"></div>
+    <div className="column-2">
+      <div className="row-1"></div>
+      <CurrentUserOptionsContainer />
+    </div>
+    <div className="column-3"></div>
   </div>
 )
 
