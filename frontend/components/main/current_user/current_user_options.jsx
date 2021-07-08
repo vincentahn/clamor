4b59100d71065b389010098e8c72a6c7d3698a7d
Vincent Ahn <jmahn97@hotmail.com>
2021-07-08 10:38:18 -0400

    Styling: Change Settings button to FontAwesome cog icon

--- frontend/components/main/current_user/current_user_options.jsx
+++ frontend/components/main/current_user/current_user_options.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
 
 class CurrentUserOptions extends React.Component{
   render(){
     return(
       <div className="current-user-options">
-        <h1>Hello, {this.props.username}</h1>
         <img src={this.props.profileUrl} alt="Profile Photo not Found" />
-        <button onClick={this.props.openUserForm}>Settings</button>
+        <h1>{this.props.username}</h1>
+        <a onClick={this.props.openUserForm}>
+          <div className="settings-icon">
+            <FontAwesomeIcon icon={faCog} />
+          </div>
+        </a>
       </div>
     );
   }
