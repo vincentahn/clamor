8b598a3792d2ed644700cb9fbc904771e4742e46
Vincent Ahn <jmahn97@hotmail.com>
2021-07-07 20:35:56 -0400

    Remove console.log for Heroku deployment

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -36,7 +36,6 @@ export const login = user => dispatch => (
   SessionApiUtil.login(user)
     .then(
       data => {
-        console.log(data);
         dispatch(receiveServers(data.servers));
         return dispatch(receiveCurrentUser(data.session));
       },
