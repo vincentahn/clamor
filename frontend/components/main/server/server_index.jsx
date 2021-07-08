c89899a8ca9cbd0446a17ac1b245ec8c73e5350b
Vincent Ahn <jmahn97@hotmail.com>
2021-07-08 14:49:15 -0400

    Feature: Added Context menu for server icons

--- frontend/components/main/server/server_index.jsx
+++ frontend/components/main/server/server_index.jsx
@@ -7,7 +7,45 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 class ServerIndex extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      dropdown: {
+        open: false,
+        id: 0,
+        left: 0,
+        right: 0
+      }
+    }
+    this.openContextMenu = this.openContextMenu.bind(this);
+    this.closeContextMenu = this.closeContextMenu.bind(this);
     this.handleServerCreate = this.handleServerCreate.bind(this);
+  }
+
+  openContextMenu(id){
+    return e => {
+      e.preventDefault();
+
+      this.setState({
+        dropdown: {
+          open: true,
+          id,
+          left: e.pageX,
+          top: e.pageY
+        }
+      });
+    }
+  }
+
+  closeContextMenu(e){
+    e.preventDefault();
+    
+    this.setState({
+      dropdown: {
+        open: false,
+        id: 0,
+        left: 0,
+        top: 0
+      }
+    });
   }
 
   handleServerCreate(e){
@@ -17,7 +55,13 @@ class ServerIndex extends React.Component{
 
   render(){
     const serverLinks = this.props.servers.map(server => (
-      <div key={server.id} className="server-index-item" title={server.name}>
+      <div 
+        key={server.id} 
+        className="server-index-item" 
+        title={server.name}
+        onContextMenu={this.openContextMenu(server.id)}
+        onBlur={this.closeContextMenu}>
+
         <Link to={`/channels/${server.id}`}>
           <img 
             src={server.profileUrl} 
@@ -41,6 +85,26 @@ class ServerIndex extends React.Component{
             <FontAwesomeIcon icon={faPlus} className="add-server-icon" />
           </a>
         </div>
+
+        { this.state.dropdown.open && this.state.dropdown.id
+          ? (
+            <div 
+              className="server-context-menu"
+              style={{ left: this.state.dropdown.left, top: this.state.dropdown.top }}>
+              <ul>
+                <a>
+                  <li>Edit Server</li>
+                </a>
+                <a>
+                  <li>Leave Server</li>
+                </a>
+                <a>
+                  <li>Delete Server</li>
+                </a>
+              </ul>
+            </div>
+          ) : null
+        }
       </div>
     );
   }
