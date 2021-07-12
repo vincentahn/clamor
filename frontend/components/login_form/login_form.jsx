f12faf012947f703908b96c9c239a2c73d6d28cd
Vincent Ahn <jmahn97@hotmail.com>
2021-07-12 09:13:40 -0400

    Syling: Updated Splash and Auth Forms

--- frontend/components/login_form/login_form.jsx
+++ frontend/components/login_form/login_form.jsx
@@ -111,59 +111,6 @@ class LoginForm extends React.Component{
         </div>
       </div>
     );
-
-    // return(
-    //   <div className="session-page">
-    //     <div className="session-form">
-    //       <div className="form-side">
-    //         <form onSubmit = {this.handleSubmit}>
-    //           <h1>Welcome back!</h1>
-    //           <h2>We're so excited to see you again!</h2>
-              
-    //           <h3>Email</h3>
-    //           {errors}
-    //           <input 
-    //             type="text" 
-    //             value={this.state.email}
-    //             onChange={this.update('email')}/>
-
-    //           <h3>Password</h3>
-    //           {errors}
-    //           <input 
-    //             type="password" 
-    //             value={this.state.password}
-    //             onChange={this.update('password')}/>
-
-    //           <br/>
-
-    //           <button className="session-button" type="submit">Login</button>
-    //         </form>
-            
-    //         <h2>Need an account? <Link to="/signup">Register</Link></h2>
-    //       </div>
-    //       <div className="demo-side">
-    //         <div>
-    //           <h3>
-    //             Don't want to create an account?
-    //           </h3>
-    //           <button 
-    //             className="demo-button"
-    //             onClick= {this.handleDemoLogin}>
-    //             Demo User Login
-    //           </button>
-    //         </div>
-    //       </div>
-    //     </div>
-
-    //     {/* <div className="attribution">
-    //       <h4>
-    //         <a href="https://unsplash.com/@korpa">
-    //           Header by Jr Korpa
-    //         </a>
-    //       </h4>
-    //     </div> */}
-    //   </div>
-    // );
   }
 }
 
