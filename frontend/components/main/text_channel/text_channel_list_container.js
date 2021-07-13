d85a209d6b5996d74cfb10d10bb58e7f0a1551c9
Vincent Ahn <jmahn97@hotmail.com>
2021-07-12 20:48:38 -0400

    Heroku: Remove console.log for heroku deployment

--- frontend/components/main/text_channel/text_channel_list_container.js
+++ frontend/components/main/text_channel/text_channel_list_container.js
@@ -2,9 +2,6 @@ import { connect } from "react-redux";
 import TextChannelList from "./text_channel_list";
 
 const mapStateToProps = (store, ownProps) => {
-  console.log(store);
-  console.log(ownProps);
-
   const server = store.entities.servers[ownProps.serverId];
   const textChannels = server.channel_ids.map(id => store.entities.textChannels[id]);
 
