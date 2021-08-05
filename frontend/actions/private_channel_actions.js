6312ff47fb722512797b71e2388050e23e7b10c1
Vincent Ahn <jmahn97@hotmail.com>
2021-08-05 17:55:18 -0400

    Add dispatch for private channel actions

--- frontend/actions/private_channel_actions.js
+++ frontend/actions/private_channel_actions.js
@@ -1,15 +1,35 @@
 import * as PrivateChannelApiUtil from "../util/private_channel_api_utils";
+import { receiveServerError } from "./error_actions";
+import { receiveMessages } from "./message_actions";
 
-export const fetchChannels = (currentUserId = 1) => {
+export const RECEIVE_PRIVATE_CHANNELS = "RECEIVE_PRIVATE_CHANNELS";
+export const RECEIVE_PRIVATE_CHANNEL = "RECEIVE_PRIVATE_CHANNEL";
+
+const receivePrivateChannels = channels => ({
+  type: RECEIVE_PRIVATE_CHANNELS,
+  channels
+})
+
+const receivePrivateChannel = channel => ({
+  type: RECEIVE_PRIVATE_CHANNEL,
+  channel
+})
+
+export const fetchChannels = (currentUserId = 1) => dispatch => {
   PrivateChannelApiUtil.fetchPrivateChannels(currentUserId)
-    .then(data => {
-      console.log(data);
-    });
+    .then(
+      channels => dispatch(receivePrivateChannels(channels)),
+      errors => dispatch(receiveServerError(errors))
+    );
 }
 
 export const fetchChannelByUser = (currentUserId = 1, otherUserId = 2) => {
   PrivateChannelApiUtil.fetchPrivateChannelByUser(currentUserId, otherUserId)
-    .then(data => {
-      console.log(data);
-    });
+    .then(
+      data => {
+        dispatch(receivePrivateChannel(data.channel));
+        dispatch(receiveMessages(data.messages));
+      },
+      errors => dispatch(receiveServerError(errors))
+    );
 }
--- frontend/clamor.jsx
+++ frontend/clamor.jsx
@@ -4,15 +4,10 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
-import { fetchChannelByUser, fetchChannels } from './actions/private_channel_actions';
-// import { fetchPrivateChannelByUser } from './util/private_channel_api_utils';
-
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
 
   window.store = store;
-  window.fetchChannels = fetchChannels;
-  window.fetchChannel = fetchChannelByUser;
 
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
